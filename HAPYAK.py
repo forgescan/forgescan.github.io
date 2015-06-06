d6a926a2721e4ab7f5de9d935fea50fa8ee2ac0d
Ubuntu <ubuntu@ip-172-31-18-114.us-west-2.compute.internal>
2015-06-06 19:10:16 +0000

    hapyak

--- HAPYAK.py
+++ HAPYAK.py
@@ -1,2 +1,17 @@
+#!/usr/bin/python
+
+import os
+import logging
+logging.basicConfig(filename='testlog.log',level=logging.DEBUG)
+import traceback
+logging.debug("filestarted imported")
+
+import time
+cwd=os.getcwd()
+os.mkdir(cwd+"/testdir")
+
 while True:
+    logging.debug("here im logingincrap")
+    time.sleep(1)
     pass
+
