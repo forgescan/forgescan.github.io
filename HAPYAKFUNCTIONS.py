ab469c54f4652eb76d9db6795393762010b249c8
root <root@ip-172-31-18-114.us-west-2.compute.internal>
2015-06-07 16:02:59 +0000

    hapyak

--- HAPYAKFUNCTIONS.py
+++ HAPYAKFUNCTIONS.py
@@ -189,7 +189,7 @@ def gitupdate():
 
 
         #os.system("git add .")
-        p = subprocess.Popen(['git','add','.'], stdout=subprocess.PIPE,stderr=subprocess.PIPE,**kwargs)
+        p = subprocess.Popen(['git','add','.'],shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE,**kwargs)
         out, err = p.communicate()
         print out
         logging.debug(str(out))
@@ -197,14 +197,14 @@ def gitupdate():
 
         #os.system("git commit -a -m hapyak")
 
-        p = subprocess.Popen(['git','commit','-a','-m','hapyak'], stdout=subprocess.PIPE,stderr=subprocess.PIPE,**kwargs)
+        p = subprocess.Popen(['git','commit','-a','-m','hapyak'],shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE,**kwargs)
         out, err = p.communicate()
         print out
         logging.debug(str(out))
         #logging.debug(str(traceback.format_exc()))
 
         #os.system("git push -f")
-        p = subprocess.Popen(['git','push','-f'], stdout=subprocess.PIPE,stderr=subprocess.PIPE,**kwargs)
+        p = subprocess.Popen(['git','push','-f'],shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE,**kwargs)
         out, err = p.communicate()
         print out
         logging.debug(str(out))
