657e9b7c3877d4ac958968158a7084dc76fd0210
root <root@ip-172-31-18-114.us-west-2.compute.internal>
2015-06-07 16:26:18 +0000

    hapyak

--- HAPYAKFUNCTIONS.py
+++ HAPYAKFUNCTIONS.py
@@ -4,6 +4,8 @@ import urllib, urllib2, cookielib
 import traceback
 import gspread002
 import logging
+import time
+
 
 
 
@@ -204,6 +206,8 @@ def gitupdate():
         #logging.debug(str(traceback.format_exc()))
 
         #os.system("git push -f")
+        os.system("git push -f")
+        time.sleep(2)
         p = subprocess.Popen(['git push -f'],shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE,**kwargs)
         out, err = p.communicate()
         print out
