eba363ebcf0cd849430caa87bb6cc4cc6bf588d2
root <root@ip-172-31-28-233.us-west-2.compute.internal>
2015-06-19 17:45:38 +0000

    hapyak

--- screenshottaker.py
+++ screenshottaker.py
@@ -3,7 +3,11 @@ import urllib
 import sys
 ##screenshottaker001
 try:
-    sys.call("sudo Xvfb :5 -screen 0 720x464x24 & ") #initiate virtual display buffer
+    os.system("sudo Xvfb :5 -screen 0 720x464x24 & ") #initiate virtual display buffer
+except:pass
+
+try:
+    #os.system("sudo Xvfb :5 -screen 0 720x464x24 & ") #initiate virtual display buffer
     cwd=os.getcwd()
     #print os.listdir(cwd+'/web')
     firmlist=os.listdir(cwd+'/web')
@@ -16,9 +20,9 @@ try:
             LandingPageURL="http://forgescan.github.io/web/"+urllib.quote(firm)+"/"+urllib.quote(firm[0])+".html"
             firmfolder =os.listdir(cwd+'//web//'+str(firm))
 
-            sys.call("sudo DISPLAY=:5 google-chrome --kiosk --window-size=720,464 --window-position=0,0 "+LandingPageURL+ "&")
+            os.system("sudo DISPLAY=:5 google-chrome --kiosk --window-size=720,464 --window-position=0,0 "+LandingPageURL+ "&")
             #delays would be placed here
-            sys.call("sudo DISPLAY=:5 import -window root "+cwd+"/web/"+firm+"/"+firm+".png")
+            os.system("sudo DISPLAY=:5 import -window root "+cwd+"/web/"+firm+"/"+firm+".png")
 
 
 
