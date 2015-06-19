9406b15a73cb0e8a1437bced256da8877b7fdd1f
root <root@ip-172-31-28-233.us-west-2.compute.internal>
2015-06-19 17:41:02 +0000

    hapyak

--- screenshottaker.py
+++ screenshottaker.py
@@ -1,8 +1,9 @@
 import os
 import urllib
-import system
+import sys
 ##screenshottaker001
 try:
+    sys.call("sudo Xvfb :5 -screen 0 720x464x24 & ") #initiate virtual display buffer
     cwd=os.getcwd()
     #print os.listdir(cwd+'/web')
     firmlist=os.listdir(cwd+'/web')
@@ -12,8 +13,15 @@ try:
         try:
             print firm
             print cwd+'/web/'+str(firm)
-            LandingPageURL="http://forgescan.github.io/web/"+urllib.quote()+"/"+urllib.quote(firm[0])+".html"
+            LandingPageURL="http://forgescan.github.io/web/"+urllib.quote(firm)+"/"+urllib.quote(firm[0])+".html"
             firmfolder =os.listdir(cwd+'//web//'+str(firm))
+
+            sys.call("sudo DISPLAY=:5 google-chrome --kiosk --window-size=720,464 --window-position=0,0 "+LandingPageURL+ "&")
+            #delays would be placed here
+            sys.call("sudo DISPLAY=:5 import -window root "+cwd+"/web/"+firm+"/"+firm+".png")
+
+
+
             ##gonna update all firms for now
             
         except:pass
