8beb13a5fc1d06bb495d14c04d81e59e5e3b393b
root <root@ip-172-31-21-56.us-west-2.compute.internal>
2015-06-20 18:48:50 +0000

    hapyak

--- screenshottaker.py
+++ screenshottaker.py
@@ -28,7 +28,7 @@ try:
 
         firm=firm.split(',')
         print firm
-        if firm[2]!="":
+        if firm[2]!='':
             print firm[2]
             """
             EmbeddedVideoURL=HAPYAKFUNCTIONS.URLbuilder(firm)
