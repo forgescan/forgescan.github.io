71d75baaf1701e99dbfaa0bea6a04e391d1c574b
root <root@ip-172-31-21-56.us-west-2.compute.internal>
2015-06-24 13:07:32 +0000

    hapyak

--- screenshottaker.py
+++ screenshottaker.py
@@ -53,8 +53,8 @@ try:
             print LandingPageURL
             firmfolder =os.listdir(cwd+'//web//'+str(firm))
             #time.sleep(1)
-            os.system("sudo DISPLAY=:5 google-chrome --incognito --kiosk --window-size=1024,600 --window-position=0,0 '"+EmbeddedVideoURL+ "'&")  #--make-default-browser
-            #windowsizewas 720,464
+            os.system("sudo DISPLAY=:5 google-chrome  --kiosk --window-size=1024,600 --window-position=0,0 '"+EmbeddedVideoURL+ "'&")  #--make-default-browser
+            #windowsizewas 720,464  and had argument --incognito
             time.sleep(8)
             #delays would be placed here
             os.system("sudo DISPLAY=:5 import -window root '"+cwd+"web/"+firm+"/"+firm+".png'")
