afbe00ee3dae60126ed1bfc73a1b52b3fe6e0e32
root <root@ip-172-31-21-56.us-west-2.compute.internal>
2015-06-24 19:44:38 +0000

    hapyak

---
+++ s3interface.py
@@ -0,0 +1,7 @@
+from boto.s3.connection import S3Connection
+conn = S3Connection(AWS_KEY, AWS_SECRET)
+bucket = conn.get_bucket(BUCKET)
+destination = bucket.new_key()
+destination.name = filename
+destination.set_contents_from_file(myfile)
+destination.make_public()
