d3016513e57bba7df8dc0507f42dec28904ca0f7
root <root@ip-172-31-21-56.us-west-2.compute.internal>
2015-06-24 17:07:55 +0000

    hapyak

--- screenshottaker.py
+++ screenshottaker.py
@@ -28,7 +28,7 @@ try:
         try:
             os.system("sudo Xvfb :5 -screen 0 1024x1200x24 & ") #initiate virtual display buffer
             os.system("""sudo sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' /root/.config/google-chrome/Default/Preferences""")
-            os.system("""sudo sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/google-chrome/Default/Preferences""")
+            os.system("""sudo sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' /root/.config/google-chrome/Default/Preferences""")
             #the 2 lines above should fix chrome
             #was 720x464x24
             pass
