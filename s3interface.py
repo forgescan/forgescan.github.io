0d66a48ea4f597d3cbb9ca4b4547303e3482f4bc
root <root@ip-172-31-21-56.us-west-2.compute.internal>
2015-06-24 19:58:24 +0000

    hapyak

--- s3interface.py
+++ s3interface.py
@@ -1,5 +1,5 @@
 from boto.s3.connection import S3Connection
-awskeystore=open("/root/ubuntu/rootkey.csv")
+awskeystore=open("/root/home/ubuntu/rootkey.csv")
 awskeystore=awskeystore.read()
 print awskeystore
 conn = S3Connection(AWS_KEY, AWS_SECRET)
