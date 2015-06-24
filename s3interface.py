dc8ad1db97364be9170a778c7d0f5760a1848131
root <root@ip-172-31-21-56.us-west-2.compute.internal>
2015-06-24 20:06:54 +0000

    hapyak

--- s3interface.py
+++ s3interface.py
@@ -7,6 +7,8 @@ AWS_SECRET=awskeystore[1].split("=")[1]
 
 print awskeystore
 conn = S3Connection(AWS_KEY, AWS_SECRET)
+BUCKET=hapyakdemopagedrop
+
 bucket = conn.get_bucket(BUCKET)
 destination = bucket.new_key()
 destination.name = filename
