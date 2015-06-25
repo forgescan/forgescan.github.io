37d8bcbbb5dc51fcefa77ac6b1dd236d2a56e28a
root <root@ip-172-31-21-56.us-west-2.compute.internal>
2015-06-25 15:03:00 +0000

    hapyak

--- HAPYAKDEMOPAGEMAKER.py
+++ HAPYAKDEMOPAGEMAKER.py
@@ -68,10 +68,10 @@ def iterateandreplace(keyword, function, worksheet):
                 #addfailedtolist
                 #errorlogging
             #cell_list+="<Cell R"+str(firmcount)+"C"+str(column)+" '"+str(value)+"'>"
-        for i in range(1,len(sheetdict)):  #gives us a tuple of an index and value
+        for i in range(1,len(sheetdict)-10):  #gives us a tuple of an index and value
             cell_list[i].value = value_list[i]    #use the index on cell_list and the val from cell_values
         del cell_list[0]
-        
+
         worksheet.update_cells(cell_list)
         #export list to desired column of sheet
     except:
