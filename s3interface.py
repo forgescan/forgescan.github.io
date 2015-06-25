3352f3acdf051e90bc1c0f4639087ae37b735eda
root <root@ip-172-31-21-56.us-west-2.compute.internal>
2015-06-25 10:40:18 +0000

    hapyak

--- s3interface.py
+++ s3interface.py
@@ -16,5 +16,5 @@ myhtml=open("/home/ubuntu/forgescan.github.io/templateHTML/templatehtml.html")
 bucket = conn.get_bucket(BUCKET)
 destination = bucket.new_key("something.html")
 destination.name = "PNGs/something.html"
-destination.set_contents_from_file(myhtml)
+destination.set_contents_from_file(myhtml) 
 destination.make_public()
